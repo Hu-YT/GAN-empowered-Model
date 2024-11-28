dd07a412adee42baac5418f1665f440354173417
Hu-YT <239523995@qq.com>
2024-11-28 21:59:58 +0800

    data_processing: 把mat格式的数据转化为tensor（easy but hard）

--- data_processing.py
+++ data_processing.py
@@ -1,11 +1,18 @@
 from scipy.io import loadmat
 import numpy as np
+import torch
+from sympy.codegen.ast import complex128
+
 
 # initial
-filepath = 'car1.mat'
+# filepath = 'car1.mat'
 
-def data_process():
+def data_process(filepath):
     print('Data loading...')
     data = loadmat(filepath)['data']
-    print(data.shape)
-    print(data[0, 0])
+    empty_tensor = torch.zeros((72, 17, 10, 5), dtype=torch.complex128)
+    for i in range(72):
+        for j in range(17):
+            empty_tensor[i][j] = torch.from_numpy(data[i][j])
+    data = empty_tensor
+    return data
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from mpmath.identification import transforms
+from torch.utils.data import DataLoader
 from wandb.integration.torch.wandb_torch import torch
 
 from data_processing import data_process
@@ -8,8 +9,6 @@ from gradient_penalty import get_gradient, gradient_penalty
 from loss import get_gen_loss, get_crit_loss
 import torch.nn as nn
 
-
-# data_process()
 
 # initializing parameters
 data_shape = (10, 5)
@@ -24,30 +23,35 @@ c_lambda = 10
 crit_repeats = 5
 device = 'cpu'
 
+# data_process()
+filepath = 'car1.mat'
+dataset = data_process(filepath)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+# for batch in dataloader:
 
-# initializing generator, critic and optimizers
-gen = Generator(z_dim).to(device)
-gen_opt = torch.optim.Adam(gen.parameters(), lr=lr, betas=(beta_1, beta_2))
-crit = Critic().to(device)
-crit_opt = torch.optim.Adam(crit.parameters(), lr=lr, betas=(beta_1, beta_2))
-
-def weights_init(m):
-    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
-        torch.nn.init.normal_(m.weight, 0.0, 0.02)
-    if isinstance(m, nn.BatchNorm2d):
-        torch.nn.init.normal_(m.weight, 0.0, 0.02)
-        torch.nn.init.constant_(m.bias, 0.0)
-gen = gen.apply(weights_init)
-crit = crit.apply(weights_init)
-
-# put it all together
-cur_step = 0
-generator_losses = []
-critic_losses = []
-
-for epoch in range(n_epochs):
-    for real, _ in (dataloder):
-        cur_batch_size = len(real)
-        real = real.to(device)
-        mean_iteration_critic_loss = 0
-        for _ in range(crit_repeats):
+# # initializing generator, critic and optimizers
+# gen = Generator(z_dim).to(device)
+# gen_opt = torch.optim.Adam(gen.parameters(), lr=lr, betas=(beta_1, beta_2))
+# crit = Critic().to(device)
+# crit_opt = torch.optim.Adam(crit.parameters(), lr=lr, betas=(beta_1, beta_2))
+#
+# def weights_init(m):
+#     if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+#         torch.nn.init.normal_(m.weight, 0.0, 0.02)
+#     if isinstance(m, nn.BatchNorm2d):
+#         torch.nn.init.normal_(m.weight, 0.0, 0.02)
+#         torch.nn.init.constant_(m.bias, 0.0)
+# gen = gen.apply(weights_init)
+# crit = crit.apply(weights_init)
+#
+# # put it all together
+# cur_step = 0
+# generator_losses = []
+# critic_losses = []
+#
+# for epoch in range(n_epochs):
+#     for real, _ in (dataloder):
+#         cur_batch_size = len(real)
+#         real = real.to(device)
+#         mean_iteration_critic_loss = 0
+#         for _ in range(crit_repeats):
